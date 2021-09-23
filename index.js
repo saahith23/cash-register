d34b5adc5927e4bd77ea0cbcda92484ed603ad41
saahith23 <saahithvaspari23@gmail.com>
2021-09-23 12:44:37 +0530

    minor

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const billAmount = document.querySelector("#bill-amount");
 const cashGiven=document.querySelector("#cash-given");
 const checkButton= document.querySelector("#check-button");
 const message = document.querySelector("#error-message");
+const numberOfNotes=document.querySelector("#no-of-notes")
 
+const availableNotes=[2000, 500, 100,20,10,5,1];
 
 checkButton.addEventListener("click", function validateBillAmountAndCashAmount()
 {
@@ -15,15 +17,22 @@ calculateChange(amountToBeReturned)
 showMessage=("the cash provided should atleast be equal to the bill amount");
 }
     }
-
-
 else{
    showMessage("Invalid bill Amount");
-});
+}
+ } );
 
-// function calculateChange(){
+function calculateChange(){
+    for (let i=0; i<availableNotes.length;i++){
+        const numberOfNotes= Math.trunc(
+            amountToBeReturned/availableNotes[i]
+        );
 
-// }
+        amountToBeReturned%= availableNotes[i];
+        noOfNotes[i].innerText=numberOfNotes;
+    }
+
+}
 function hideMessage(){
     message.style.display ="none";
 }
