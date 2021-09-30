3fd4fdc4d576804342969d5929813276ffa30e81
saahith23 <saahithvaspari23@gmail.com>
2021-09-30 12:16:24 +0530

    .

--- index.js
+++ index.js
@@ -4,17 +4,20 @@ const checkButton= document.querySelector("#check-button");
 const message = document.querySelector("#error-message");
 const numberOfNotes=document.querySelector("#no-of-notes")
 
-const availableNotes=[2000, 500, 100,20,10,5,1];
+const availableNotes=[2000, 500, 100,50,20,10,5,1];
 
 checkButton.addEventListener("click", function validateBillAmountAndCashAmount()
 {
    hideMessage();
- if (billAmount.value > 0) {
+ if (billAmount.value > 0) 
+ {
+
 if (cashGiven.value>= billAmount.value){
 const amountToBeReturned = cashGiven.value- billAmount.value;
 calculateChange(amountToBeReturned)
-}else{
-showMessage=("the cash provided should atleast be equal to the bill amount");
+}
+else{
+showMessage("the cash provided should atleast be equal to the bill amount");
 }
     }
 else{
@@ -22,13 +25,18 @@ else{
 }
  } );
 
-function calculateChange(){
+
+
+
+
+
+function calculateChange(amountToBeReturned){
     for (let i=0; i<availableNotes.length;i++){
         const numberOfNotes= Math.trunc(
             amountToBeReturned/availableNotes[i]
         );
 
-        amountToBeReturned%= availableNotes[i];
+        amountToBeReturned= amountToBeReturned% availableNotes[i];
         noOfNotes[i].innerText=numberOfNotes;
     }
 
