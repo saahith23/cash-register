93f326b931d15453705072b86547eaded9f7b76f
saahith23 <saahithvaspari23@gmail.com>
2021-09-30 23:07:22 +0530

    .

--- index.js
+++ index.js
@@ -1,51 +1,47 @@
 const billAmount = document.querySelector("#bill-amount");
-const cashGiven=document.querySelector("#cash-given");
-const checkButton= document.querySelector("#check-button");
+const cashGiven = document.querySelector("#cash-given");
+const checkButton = document.querySelector("#check-button");
 const message = document.querySelector("#error-message");
-const numberOfNotes=document.querySelector("#no-of-notes")
+const numberOfNotes = document.querySelectorAll("#no-of-notes")
 
-const availableNotes=[2000, 500, 100,50,20,10,5,1];
+const availableNotes = [2000, 500, 100, 50, 20, 10, 5, 1];
 
-checkButton.addEventListener("click", function validateBillAmountAndCashAmount()
-{
-   hideMessage();
- if (billAmount.value > 0) 
- {
+checkButton.addEventListener("click", function validateBillAmountAndCashAmount() {
+    hideMessage();
+    if (billAmount.value > 0) {
 
-if (cashGiven.value>= billAmount.value){
-const amountToBeReturned = cashGiven.value- billAmount.value;
-calculateChange(amountToBeReturned)
-}
-else{
-showMessage("the cash provided should atleast be equal to the bill amount");
-}
+        if (cashGiven.value >= billAmount.value) {
+            const amountToBeReturned = cashGiven.value - billAmount.value;
+            calculateChange(amountToBeReturned)
+        } else {
+            showMessage("the cash provided should atleast be equal to the bill amount");
+        }
+    } else {
+        showMessage("Invalid bill Amount");
     }
-else{
-   showMessage("Invalid bill Amount");
-}
- } );
+});
 
 
-
-
-
-
-function calculateChange(amountToBeReturned){
-    for (let i=0; i<availableNotes.length;i++){
-        const numberOfNotes= Math.trunc(
-            amountToBeReturned/availableNotes[i]
+function calculateChange(amountToBeReturned) {
+    for (let i = 0; i < availableNotes.length; i++) {
+        const noOfNotes = Math.trunc(
+            amountToBeReturned / availableNotes[i]
         );
-
-        amountToBeReturned= amountToBeReturned% availableNotes[i];
-        noOfNotes[i].innerText=numberOfNotes;
+        
+        amountToBeReturned = amountToBeReturned % availableNotes[i];
+       
+    
+        numberOfNotes[i].innerText = noOfNotes;
     }
 
 }
-function hideMessage(){
-    message.style.display ="none";
+
+function hideMessage() {
+    message.style.display = "none";
 }
-function showMessage(msg){
-    message.style.display="block";
-    message.innerText=msg;
+
+function showMessage(msg) {
+    message.style.display = "block";
+    message.innerText = msg;
 
 }
